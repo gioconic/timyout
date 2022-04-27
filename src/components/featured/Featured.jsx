fa53a43b7599bc9508fc1a92779ea9655f49967e
Athena <giovanni.gab@icloud.com>
2022-04-27 22:41:46 +0200

    featured chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,50 @@
 import "./featured.scss";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const Featured = () => {
-  return <div className="featured"></div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Workers</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="title">Total workers today</p>
+        <p className="amount">420</p>
+        <p className="desc">Last hour data not included.</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">600</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpIcon fontSize="small" />
+              <div className="resultAmount">605</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpIcon fontSize="small" />
+              <div className="resultAmount">560</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
